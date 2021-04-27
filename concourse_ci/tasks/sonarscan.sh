78c98a17e78355fdb13561a8cf405f0faf3a741b
Gaurav <g.gupta@hcl.com>
2021-04-27 17:52:59 -0500

    changes

--- concourse_ci/tasks/sonarscan.sh
+++ concourse_ci/tasks/sonarscan.sh
@@ -19,9 +19,9 @@ ls -lt
 chmod +x concourse_ci/tasks/sonarscan.sh
 chmod +x ./mvnw
 
-./mvnw test
-#./mvnw package --s settings.xml -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dmaven.test.skip=true sonar:sonar -Dsonar.host.url=http://192.168.0.23:9000/ -Dsonar.verbose=true -Dsonar.projectKey=springboot_sample -Dsonar.projectName=springboot_sample -Dsonar.exclusions=src/main/java/com/pepsico/**Factory.java,src/main/java/com/pepsico/**Constant.java,src/test/**,**.xml -Dsonar.java.binaries=target/classes
+#./mvnw test
 
+#./mvnw package --s settings.xml -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dmaven.test.skip=true sonar:sonar -Dsonar.host.url=http://192.168.0.23:9000/ -Dsonar.verbose=true -Dsonar.projectKey=springboot_sample -Dsonar.projectName=springboot_sample -Dsonar.exclusions=src/main/java/com/pepsico/**Factory.java,src/main/java/com/pepsico/**Constant.java,src/test/**,**.xml -Dsonar.java.binaries=target/classes
 #./mvnw package -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dmaven.test.skip=true sonar:sonar -Dsonar.host.url=http://192.168.0.23:9000/ -Dsonar.verbose=true -Dsonar.projectKey=springboot_sample -Dsonar.projectName=springboot_sample -Dsonar.exclusions=src/main/java/com/pepsico/**Factory.java,src/main/java/com/pepsico/**Constant.java,src/test/**,**.xml -Dsonar.java.binaries=target/classes
 
 
