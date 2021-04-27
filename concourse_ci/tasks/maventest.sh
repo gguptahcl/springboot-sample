63c1a48042532ef8b633e84a149db2fb2ffe4f57
Gaurav <g.gupta@hcl.com>
2021-04-27 18:09:34 -0500

    changes

--- concourse_ci/tasks/maventest.sh
+++ concourse_ci/tasks/maventest.sh
@@ -9,13 +9,12 @@ ls -lt
 chmod +x concourse_ci/tasks/maventest.sh
 chmod +x ./mvnw
 
-
+echo "Present working directory is...."
 echo "$(pwd)"
 
 # ./mvnw test --s settings.xml -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true
 
 ./mvnw test
-
 
 cp -R target/test spring-boot-sample-unit-test-output
 
