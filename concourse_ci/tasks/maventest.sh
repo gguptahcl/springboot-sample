ad63a2695ab20c3cbb386082a4c096e2581f0557
Gaurav <g.gupta@hcl.com>
2021-04-27 18:27:10 -0500

    changes

--- concourse_ci/tasks/maventest.sh
+++ concourse_ci/tasks/maventest.sh
@@ -14,7 +14,7 @@ echo "$(pwd)"
 
 ./mvnw clean --s settings.xml -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true
 
-#./mvnw clean --s settings.xml
+#./mvnw clean test --s settings.xml
 
 cp -R target/test spring-boot-sample-unit-test-output
 
